326126916f28e9f5dc3fe269e2a277cb757cf266
koisland <koshima789@gmail.com>
2025-12-03 10:11:36 -0500

    Index bam.

--- 2-align.sh
+++ 2-align.sh
@@ -8,3 +8,5 @@ wget https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/HG002/assemblies/h
 # conda install bioconda::minimap2 bioconda::samtools
 minimap2 -x lr:hq data/assembly/hg002v1.1.fasta.gz <(samtools bam2fq -T "*" data/ubam/*.bam) -t 12 -K 8G | \
     samtools view -bh -o results/alignment/HG002.bam
+
+samtools index results/alignment/HG002.bam
